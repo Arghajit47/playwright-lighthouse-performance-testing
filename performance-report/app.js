a64fe968ae894c941256ad475b7953475eb5ea5f
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2024-11-17 13:37:12 +0530

    Added new performance testing reporter

--- performance-report/app.js
+++ performance-report/app.js
@@ -8,44 +8,29 @@ const path = require("path");
  * @param {string} dirPath - The directory path to read.
  * @returns {object} - The folder structure as a JSON object.
  */
-function getFolderStructure(dirPath) {
-  const structure = {};
 
-  fs.readdirSync(dirPath, { withFileTypes: true }).forEach((entry) => {
-    const entryPath = path.join(dirPath, entry.name);
-
-    if (entry.isDirectory()) {
-      // Recursively get the structure of the subdirectory
-      structure[entry.name] = getFolderStructure(entryPath);
-    } else {
-      // Add the file to the structure
-      structure[entry.name] = "file";
-    }
-  });
-
-  return structure;
-}
 
 // Usage
-const rootPath = path.join(__dirname, ""); // Replace 'reports' with your directory
-const folderStructure = getFolderStructure(rootPath);
+function readFolderStructure(folderPath) {
+  const stats = fs.statSync(folderPath);
 
-// Output the structure as JSON
-console.log(JSON.stringify(folderStructure, null, 2));
-
-// Define file path
-const outputFilePath = path.join(__dirname, "folderStructure.json");
-
-// Write JSON data to the file
-fs.writeFile(
-  outputFilePath,
-  JSON.stringify(folderStructure, null, 2),
-  "utf8",
-  (err) => {
-    if (err) {
-      console.error("Error writing file:", err);
-    } else {
-      console.log("File successfully written to", outputFilePath);
-    }
+  if (stats.isDirectory()) {
+    const content = fs.readdirSync(folderPath);
+    return content
+      .filter((item) => item !== ".DS_Store") // Filter out .DS_Store
+      .reduce((acc, item) => {
+        const fullPath = path.join(folderPath, item);
+        acc[item] = readFolderStructure(fullPath);
+        return acc;
+      }, {});
+  } else {
+    return "file";
   }
+}
+
+const folderStructure = readFolderStructure(__dirname);
+fs.writeFileSync(
+  "folderStructure.json",
+  JSON.stringify(folderStructure, null, 2)
 );
+  console.log("Folder structure written to folderStructure.json");
