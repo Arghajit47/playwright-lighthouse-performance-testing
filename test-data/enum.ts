ff9bd314d712ff986f7bfa41643e701fe40b2fab
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2025-01-26 20:16:56 +0530

    Refactor Playwright workflow and performance tests; remove unused artifact uploads and deploy steps, update test scripts for single worker execution, and add HTML attachment functionality to Allure reports.

--- test-data/enum.ts
+++ test-data/enum.ts
@@ -1,9 +1,9 @@
 export const URLS = {
   NEXTBNB_BASEURL: "https://nextbnb-three.vercel.app/",
-  POOL: "https://nextbnb-three.vercel.app/?category=Pools",
-  BEACH: "https://nextbnb-three.vercel.app/?category=Beach",
-  CAVES: "https://nextbnb-three.vercel.app/?category=Caves",
-  LUXARY: "https://nextbnb-three.vercel.app/?category=Lux",
+  // POOL: "https://nextbnb-three.vercel.app/?category=Pools",
+  // BEACH: "https://nextbnb-three.vercel.app/?category=Beach",
+  // CAVES: "https://nextbnb-three.vercel.app/?category=Caves",
+  // LUXARY: "https://nextbnb-three.vercel.app/?category=Lux",
 };
 
 export const UNAUTHORIZED_PATHS = {
